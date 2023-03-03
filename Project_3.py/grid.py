fb5a0096b01e1ad2edd238fd7e11ef6247ddc25b
Albert <c.albertj26@gmail.com>
2023-03-03 11:32:19 -0400

    Adding methods for the matrix

--- Project_3.py/grid.py
+++ Project_3.py/grid.py
@@ -1,25 +1,35 @@
-class Grid:
-    def __init__(self, rows, colums, values = None):
-        self.grid = []
+import numpy as np
 
-        for row in range(rows):
-            self.grid.append([])
-            for col in range(colums):
-                self.grid[row].append(values)
+class Matrix:
+    def __init__(self, n, m):
+        self.matrix = self.get_matrix(n, m)
 
-    def get_height (self):
-        return len(self.grid)
-    
-    def get_width(self):
-        return len(self.grid[0])
-    
+    def get_matrix(self,n, m):
+        num = 1
+        matrix = [[None for j in range(m)] for i in range(n)]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] = num
+                num += 1
+        return matrix
+
+    def get_readable_matrix_string(self, matrix):
+        strings = []
+        for row in matrix:
+            strings.append(str(row))
+        return '\n'.join(strings)  
+
     def __str__(self):
-        result = ""
+        return self.get_readable_matrix_string(self.matrix)
+    
+    def __len__(self):
+        return len(self.matrix)
+    
+'''M1 = Matrix(3,3)
 
-        for row in range(self.get_height()):
-            for col in range(self.get_width()):
-                result += str(self.grid[row][col]) + " "
-
-            result += "\n"
-        return str(result)
-
+M2 = Matrix(3,3)
+s = M1.get_matrix(3,3)
+a = np.array(s)
+c = M2.get_matrix(3,3)
+b = np.array(c)
+'''
---
+++ Project_3.py/operations.py
@@ -0,0 +1,23 @@
+import numpy as np
+from grid import Matrix 
+
+def sum (m1, m2):
+    return 
+
+def subtraction (m1, m2):
+    return
+
+def multiply (m1, m2):
+    return
+
+def determinant (m1):
+    return
+
+def transpose (m1):
+    return
+
+def diagonal (m1):
+    return
+
+def inverse (m1):
+    return
